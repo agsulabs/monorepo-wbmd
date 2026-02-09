c3a83989cd578147b55c37a31639b29eec14e2f0
Elvin Islamov <elvinaxsu@gmail.com>
2026-02-09 04:00:47 +0100

    fix(mobile): eslint env overrides for node and jest

--- apps/mobile/.eslintrc.js
+++ apps/mobile/.eslintrc.js
@@ -1,4 +1,24 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  overrides: [
+    {
+      files: [
+        '.eslintrc.js',
+        '.prettierrc.js',
+        'babel.config.js',
+        'jest.config.js',
+        'metro.config.js',
+        '**/*.config.js',
+      ],
+      env: { node: true },
+    },
+    {
+      files: [
+        '**/__tests__/**/*.{js,jsx,ts,tsx}',
+        '**/*.{spec,test}.{js,jsx,ts,tsx}',
+      ],
+      env: { jest: true },
+    },
+  ],
 };
